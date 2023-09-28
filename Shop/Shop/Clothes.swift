d5fe42e6e0a71ba7e85dce95cb6f38db5afcecbc
Elias Breitenbach <elias.b@me.com>
2023-09-28 21:27:33 +0200

    Create Protocols

---
+++ Shop/Shop/Clothes.swift
@@ -0,0 +1,8 @@
+//
+//  Clothes.swift
+//  Shop
+//
+//  Created by Elias Breitenbach on 28.09.23.
+//
+
+import Foundation
---
+++ Shop/Shop/Hardware.swift
@@ -0,0 +1,8 @@
+//
+//  Hardware.swift
+//  Shop
+//
+//  Created by Elias Breitenbach on 28.09.23.
+//
+
+import Foundation

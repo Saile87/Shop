b0e9e468e1e431ffd7244e8664f914c3576cf41e
Elias Breitenbach <elias.b@me.com>
2023-09-28 21:24:54 +0200

    Create a project in git hub

---
+++ Shop/Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Elias Breitenbach on 28.09.23.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

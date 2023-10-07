a765db3f2bb8d166687180848ea101e1473f4b0f
Elias Breitenbach <elias.b@me.com>
2023-10-07 22:40:53 +0200

    First try code

--- Shop/Shop/ContentView.swift
+++ Shop/Shop/ContentView.swift
@@ -9,22 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let hardware: [Hardware] = [Keyboard(id: 1, brand: "RK68", price: 50, color: "White", serialNumber: 99),
-                                Mouse(id: 2, brand: "Logitech", price: 44, Color: "Black", serialNumber: 98),
-                                CallOfDuty(id: 3, serialNumber: 97, brand: "Activison", price: 55),
-                                Fifa23(id: 4, serialNumber: 96, brand: "EASports", price: 60)
+    let hardwares: [Hardware] = [Keyboard(id: 1, brand: "RK68", price: 50, color: "White", serialNumber: 99),
+                                 Mouse(id: 2, brand: "Logitech", price: 44, Color: "Black", serialNumber: 98),
+                                 CallOfDuty(id: 3, serialNumber: 97, brand: "Activison", price: 55),
+                                 Fifa23(id: 4, serialNumber: 96, brand: "EASports", price: 60)
     ]
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                TextField("Bro", text: $hardwares)
+            }
         }
-        .padding()
     }
 }
+
+
+
+
+
 
 #Preview {
     ContentView()
